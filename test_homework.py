000d7e9054e6e692438dc9a5e911398ff2c93d92
Oksana Bondareva <xanka359@yandex.ru>
2024-02-14 21:05:59 +0300

    commit with homework

--- test_homework.py
+++ test_homework.py
@@ -7,7 +7,7 @@ def test_greeting():
     name = "Анна"
     age = 25
     # TODO Сформируйте нужную строку
-    output = ""
+    output = f"Привет, {name}! Тебе {age} лет."
 
     # Проверяем результат
     assert output == "Привет, Анна! Тебе 25 лет."
@@ -21,12 +21,12 @@ def test_rectangle():
     a = 10
     b = 20
     # TODO сосчитайте периметр
-    perimeter = 0
+    perimeter = a*2 + b*2
 
     assert perimeter == 60
 
     # TODO сосчитайте площадь
-    area = 0
+    area = a * b
 
     assert area == 200
 
@@ -36,14 +36,15 @@ def test_circle():
     Напишите программу, которая берет радиус круга и выводит на экран его длину и площадь.
     Используйте константу PI
     """
+    import math
     r = 23
     # TODO сосчитайте площадь
-    area = 0
+    area = math.pi * r**2
 
     assert area == 1661.9025137490005
 
     # TODO сосчитайте длину окружности
-    length = 0
+    length = 2* math.pi * r
 
     assert length == 144.51326206513048
 
@@ -53,8 +54,11 @@ def test_random_list():
     Создайте список из 10 случайных чисел от 1 до 100 (включая обе границы) и отсортируйте его по возрастанию.
     """
     # TODO создайте список
+    import random
     l = []
-
+    for i in range(10):
+        l.append(random.randint(1, 101))
+    l.sort()
     assert len(l) == 10
     assert all(l[i] <= l[i + 1] for i in range(len(l) - 1))
 
@@ -65,7 +69,7 @@ def test_unique_elements():
     """
     l = [1, 2, 3, 4, 5, 5, 5, 6, 7, 8, 8, 9, 10, 10]
     # TODO удалите повторяющиеся элементы
-
+    l= list(set(l))
     assert isinstance(l, list)
     assert len(l) == 10
     assert l == [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
@@ -80,8 +84,7 @@ def test_dicts():
     first = ["a", "b", "c", "d", "e"]
     second = [1, 2, 3, 4, 5]
     # TODO создайте словарь
-    d = {}
-
+    d = dict(zip(first, second))
     assert isinstance(d, dict)
     assert len(d) == 5
     assert list(d.keys()) == first
